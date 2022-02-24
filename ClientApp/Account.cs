b5947b50d5e11f0d3d72c51f865914dccd2360d9
eswar <neswar2021@gmail.com>
2022-02-24 18:04:29 +0530

    Client app added

---
+++ ClientApp/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AccountNumber { get; set; }
+    }
+}
---
+++ ClientApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ClientApp
+{
+    internal class Program
+    {
+
+
+        public async void DataPost(Account data)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44342/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                var response = client.PostAsJsonAsync("Account", data).Result;
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseData);
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("please enter user details..");
+            Console.WriteLine("enter User Name:");
+
+            var name = Console.ReadLine();
+            Console.WriteLine("enter Account Number:");
+            var accountNumber = Console.ReadLine();
+
+            var account = new Account() { Id = 0, Name = name, AccountNumber = accountNumber };
+
+            Program pr = new Program();
+
+            pr.DataPost(account);
+
+            Console.ReadLine();
+        }
+    }
+}
